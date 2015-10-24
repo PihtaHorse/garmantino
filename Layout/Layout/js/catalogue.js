d9df50646bc54b4f658305d333661c078d38155e
emamul <pihtahorse@gmail.com>
2015-10-24 18:46:08 +0300

    Item page changed

--- Layout/Layout/js/catalogue.js
+++ Layout/Layout/js/catalogue.js
@@ -1,2 +1,2 @@
-var buttonToTop = document.querySelector(".fa-angle-up");
+var buttonToTop = document.querySelector(".move-up .fa-angle-up");
 buttonToTop.addEventListener("click", function(){window.scrollTo(0, 0)});
---
+++ Layout/Layout/js/item.js
@@ -0,0 +1,58 @@
+"use strict"
+var buttonToTop = document.querySelector(".move-up .fa-angle-up");
+buttonToTop.addEventListener("click", function(){window.scrollTo(0, 0)});
+
+var i, n, last, lastNumber,
+    bigImg = document.querySelector("#mainImgWrapper"),
+    buttonUp = document.querySelector("#buttonPrevious"),
+    buttonDown = document.querySelector("#buttonNext"), 
+    imgs = document.querySelectorAll("#smallImages .smallImg"),
+    imgWrappers = document.querySelectorAll("#smallImages .smallImgWrapper");
+
+var smallImgClick = function(smallImg, i){
+    var callback = function(){
+            bigImg.style.backgroundImage = "url(" + smallImg.src + ")";
+            last.style.opacity = 0.4;
+            smallImg.style.opacity = 1;
+            last = smallImg;
+            lastNumber = i;
+        };
+    
+    return callback;
+};
+
+var buttonUpClick = function(){
+    
+    if (lastNumber != 0) {
+        bigImg.style.backgroundImage = "url(" + last.src + ")";
+        last.style.opacity = 0.4;
+        last = imgs[--lastNumber];
+        last.style.opacity = 1;
+    };
+};
+
+var buttonDownClick = function(){
+    
+    if (lastNumber != n-1) {
+        bigImg.style.backgroundImage = "url(" + last.src + ")";
+        last.style.opacity = 0.4;
+        last = imgs[++lastNumber];
+        last.style.opacity = 1;
+    };
+};
+
+buttonUp.addEventListener("click", buttonUpClick);
+buttonDown.addEventListener("click", buttonDownClick);
+
+n = imgs.length;
+
+for(i = 0; i < n; i++ ){
+    imgs[i].style.opacity = 0.4;
+    imgs[i].addEventListener("click", smallImgClick(imgs[i], i));
+}
+
+lastNumber = 0;
+if (n != 0){
+    last = imgs[0];
+    imgs[0].style.opacity = 1;
+}
